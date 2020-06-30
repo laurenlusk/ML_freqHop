9186e225fa7a02691c22483d2380b90684eb47a7
Lauren Lusk <laurenlusk77@yahoo.com>
2020-06-30 12:53:55 -0500

    frequency hopper generator

--- freqHopper.m
+++ freqHopper.m
@@ -5,16 +5,18 @@ endFreq = 2.480e9;    % highest center freq for channels
 chanBW = 1e6;   %channel bandwidth is 1MHz, sampling rate 1us
 
 hop = randperm(numChan);  % generates freq hopping pattern
+hop2 = randperm(numChan);
+hop3 = randperm(numChan);
+hop = [hop hop2 hop3];
 sampPerHop = 625;  % there are 625us/hop, 1600 hops/s
 
-occ = zeros([numChan sampPerHop*numChan]);
+occ = zeros([numChan sampPerHop*length(hop)]);
 
-for i=1:numChan     % takes channel # & makes it 1 (for occupied)
+for i=1:length(hop)     % takes channel # & makes it 1 (for occupied)
     for j=1:sampPerHop
         occ(hop(i),j+(sampPerHop*(i-1))) = 1;    % changes occupied channel's status to occupied (1)
     end
 end
 
 occ = occ';
-occ = [occ; occ];
-csvwrite("all_data2.csv",occ);
+csvwrite("3_cycles.csv",occ);
